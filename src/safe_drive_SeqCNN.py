14ccfb8cb6fd6070ec2bd81f68ad2367fe22e7ef
Vincenzo Gargano <cenzogargano@gmai.com>
2022-03-23 13:13:51 +0100

    removed batch_size from rescaling and conv2d

--- src/safe_drive_SeqCNN.py
+++ src/safe_drive_SeqCNN.py
@@ -21,13 +21,13 @@ def generate_model_safe_drive():
 
 
         #Rescaling the input image to a fixed size
-        tf.keras.layers.Rescaling(1./255, input_shape=(batch_size ,256, 256, 3)),
+        tf.keras.layers.Rescaling(1./255, input_shape=(256, 256, 3)),
 
         #Flatten the input to a 1-D vector
-        #tf.keras.layers.Flatten(input_shape=(batch_size, 256, 256, 3)),
+        #tf.keras.layers.Flatten(input_shape=(256, 256, 3)),
 
         #First convolutional layer with 32 filters and a kernel size of 3x3
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(batch_size ,img_height, img_width, 3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
 
         #Second convolutional layer with 64 filters and a kernel size of 3x3
