f8003219a3ae1e1a04a3a7c2a6ff7a33489fc974
Vincenzo Gargano <cenzogargano@gmai.com>
2022-04-02 18:35:01 +0200

    updated

--- later.py
+++ later.py
@@ -51,3 +51,88 @@ for image_batch, labels_batch in dataset_to_train:
     print(image_batch.shape)
     print(labels_batch.shape)
     break '''
+
+
+#In simulated fed learning 
+
+'''
+#Define a model in the same way it's defined int safe_drive_SeqCNN.py but with a class
+class CNNModel(epochs = 10):
+    
+    # Model for image classification on 15 classes, 
+    # classes consists in actions one of them is safe driving the other are action that distract the user
+    # We use a CNN with 3 convolutional layers and a fully connected layer, and we use a softmax activation function for the last layer.
+    def generate_model_safe_drive():
+
+        def __init__(self, name, age):
+                self.model = model
+                
+
+        model = tf.keras.Sequential([
+            #Rescaling the input image to a fixed size
+            tf.keras.layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+
+            #Flatten the input to a 1-D vector
+            #tf.keras.layers.Flatten(input_shape=(256, 256, 3)),
+
+            #First convolutional layer with 32 filters and a kernel size of 3x3
+            tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+            tf.keras.layers.MaxPooling2D(2, 2),
+
+            #Second convolutional layer with 64 filters and a kernel size of 3x3
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+
+            #Third convolutional layer with 128 filters and a kernel size of 3x3
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+
+            tf.keras.layers.Dropout(.5, input_shape=(img_height,img_width,3)),
+
+            #Flatten the output of the previous layer
+            tf.keras.layers.Flatten(),
+
+            tf.keras.layers.Dropout(.5, input_shape=(img_height,img_width,3)),
+
+            #Anothet fully connected layer with 512 units
+            tf.keras.layers.Dense(240, activation='relu'),
+
+            #Final layer with 15 classes
+            tf.keras.layers.Dense(num_classes, activation='softmax')
+        ])
+
+        model.summary()
+
+        return model
+
+    #Try experimenting with different optimizers and different optimizer configs
+    def model_compile(model):
+
+        model.compile(optimizer = 'adam', 
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+        return model
+
+    def fit_model(model):
+        model.fit(dataset_to_train, validation_data = dataset_to_validate, epochs=10)
+
+
+    def trained_model_evaluation(model):
+        test_loss, test_acc = model.evaluate(dataset_to_validate)
+        print('\nTest accuracy:', test_acc)
+
+    def get_weights(model):
+        return model.get_weights()
+
+    def start_training():
+        model = generate_model_safe_drive()
+        print("\n\n\nModel generated with success!\n\n\n")
+        model = model_compile(model)
+        print("\n\n\nModel compiled with success!\n\n\n")
+        fit_model(model)
+        print("\n\n\nModel trained with success!\n\n\n")
+        #history.results()
+        trained_model_evaluation(model)
+
+'''
