fcf87e7b02c9f61ec5da2a01f76474db0565cbe7
U-n-Own <cenzogargano@gmail.com>
2022-04-21 20:05:36 +0200

    all model lenht restraint removed

--- src/Simulated_Federation_Learning.py
+++ src/Simulated_Federation_Learning.py
@@ -63,7 +63,7 @@ PATH = '/home/gargano/dataset/dataWithoutMasks'
 USERS =['Amparore', 'Baccega', 'Basile', 'Beccuti', 'Botta', 'Castagno', 'Davide', 'DiCaro', 'DiNardo','Esposito','Francesca','Giovanni','Gunetti','Idilio','Ines','Malangone','Maurizio','Michael','MirkoLai','MirkoPolato','Olivelli','Pozzato','Riccardo','Rossana','Ruggero','Sapino','Simone','Susanna','Theseider','Thomas']
 USERS_TWO =['Amparore', 'Baccega']
 CATEGORIES = ["c00","c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13","c14"]
-all_models = list(range(0,2))
+all_models = [] 
 collaborators = []
 num_clients = len(USERS)
 
