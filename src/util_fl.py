8e99ca1bb6e265b17f2e716f4346e1e4e91c8011
U-n-Own <cenzogargano@gmail.com>
2022-06-01 16:15:28 +0000

    parameter used for last run

--- src/util_fl.py
+++ src/util_fl.py
@@ -6,7 +6,7 @@ PATH = '/home/gargano/dataset/dataWithoutMasks'
 CATEGORIES = ["c00","c01","c02","c03","c04","c05","c06","c07","c08","c09","c10","c11","c12","c13","c14"]
 NUMBER_CLASSES = 15
 USERS = ['Amparore', 'Baccega', 'Basile', 'Beccuti', 'Botta', 'Castagno', 'Davide', 'DiCaro', 'DiNardo','Esposito','Francesca','Giovanni','Gunetti','Idilio','Ines','Malangone','Maurizio','Michael','MirkoLai','MirkoPolato','Olivelli','Pozzato','Riccardo','Rossana','Ruggero','Sapino','Simone','Susanna','Theseider','Thomas']
-USERS_EXCLUDED =['Amparore']
+USERS_EXCLUDED =['Gunetti']
 
 #A lambda function that filter and exclude the n-th user of user in the for loop
 USERS_TRAINING = list(filter(lambda x: x not in USERS_EXCLUDED, USERS))
